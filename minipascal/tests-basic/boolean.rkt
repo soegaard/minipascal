17bf565f0ff1a5bbb306c5a0db4f432c2d285f15
Jens Axel Søgaard <jensaxel@soegaard.net>
2013-01-29 22:44:33 +0100

    Added basic tests for arithmetic and booleans.

---
+++ minipascal/tests-basic/boolean.rkt
@@ -0,0 +1,15 @@
+#lang minipascal
+{ Tests boolean operators
+  Expected result: alterning true, false, ...}
+program arithmetic;
+begin
+  writeln(true);
+  writeln(false);
+  writeln(not false);  
+  writeln(not true);
+  writeln(true or false);
+  writeln(true and false);
+  writeln(not (true and false));
+  writeln(not (true or false));
+end.
+  
