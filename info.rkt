11a29b35dd9d2e192179f090ea27c8ebd97c1fb5
Jens Axel Søgaard <jensaxel@soegaard.net>
2015-05-23 14:59:06 +0200

    Added dependencies

--- info.rkt
+++ info.rkt
@@ -1,9 +1,17 @@
 #lang info
 (define collection 'multi)
 (define version    "1.0")
-(define deps '("ragg"))
+(define deps '("ragg" "base"
+                      "parser-tools-lib"
+                      "unstable-list-lib"))
 
 
-(define build-deps '("at-exp-lib" "rackunit-lib" "scribble-lib" "racket-doc"))
+(define build-deps '("base"
+                      "parser-tools-lib"
+                      "unstable-list-lib"
+                      "at-exp-lib"
+                      "rackunit-lib"
+                      "scribble-lib"
+                      "racket-doc"))
 
 
