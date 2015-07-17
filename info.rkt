d3ad8f0245f431ec020dc603a4258a4513111545
Vincent St-Amour <stamourv@racket-lang.org>
2015-07-17 11:59:44 -0500

    Remove dependency on unstable-list-lib.

--- info.rkt
+++ info.rkt
@@ -2,13 +2,11 @@
 (define collection 'multi)
 (define version    "1.0")
 (define deps '("ragg" "base"
-                      "parser-tools-lib"
-                      "unstable-list-lib"))
+                      "parser-tools-lib"))
 
 
 (define build-deps '("base"
                       "parser-tools-lib"
-                      "unstable-list-lib"
                       "at-exp-lib"
                       "rackunit-lib"
                       "scribble-lib"
